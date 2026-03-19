08adbe5f569335baf088fbb8c19603236d4f4126
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-19 18:13:41 +0530

    Time: 566 ms (34.55%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,18 +1,16 @@
 # Write your MySQL query statement be
-SELECT product_id,
-       COALESCE(new_price, 10) AS price
-FROM (
-    SELECT product_id, new_price,
-           ROW_NUMBER() OVER (
-               PARTITION BY product_id
-               ORDER BY change_date DESC
-           ) AS rn
+SELECT product_id, new_price as price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date) 
     FROM Products
     WHERE change_date <= '2019-08-16'
-) t
-WHERE rn = 1
-UNION
-SELECT DISTINCT product_id, 10
+    GROUP BY product_id
+) # Till this point, we got the prices for items for which <= 16 date was present
+
+UNION 
+
+SELECT product_id, 10 
 FROM Products
 WHERE product_id NOT IN (
     SELECT product_id
