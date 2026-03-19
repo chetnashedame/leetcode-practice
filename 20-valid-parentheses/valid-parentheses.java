7fc3a1a9b7370fed03c66f85bcb661d1ab94f835
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-20 00:39:07 +0530

    Time: 3 ms (87.45%) | Memory: 43.1 MB (82.52%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch =='['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty())return false;
+
+                char topmost=stack.pop();
+
+                if(ch==')'&& topmost!='(' || ch=='}'&& topmost!='{' || ch==']'&& topmost!='[' ){
+                return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
