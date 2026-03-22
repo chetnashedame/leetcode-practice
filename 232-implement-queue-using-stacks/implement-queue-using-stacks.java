819044eb4cae83fd83aac2d0c0694e496578df30
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-23 01:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (52.61%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,31 @@
+class MyQueue {
+
+    Stack<Integer> in = new Stack<>();
+    Stack<Integer> out = new Stack<>();
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    public int peek() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
