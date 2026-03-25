366d720469683e80c02f67fdc96f8048ade8c118
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-25 22:51:05 +0530

    Time: 20 ms (95.77%) | Memory: 60.5 MB (77.46%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -5,6 +5,8 @@ class Solution {
     if(n<=4) return 0;
 
         Arrays.sort(nums);
+     //Remove 3 → try all splits:
+//(3,0), (2,1), (1,2), (0,3)
 
         int a=nums[n-4]-nums[0];
         int b=nums[n-3]-nums[1];
