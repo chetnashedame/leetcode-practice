3e0f6ae402874340cb72dba9444ce4d12c2b8038
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-26 17:14:25 +0530

    Time: 32 ms (52.05%) | Memory: 48.3 MB (6.26%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.java
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.java
@@ -1,3 +1,13 @@
+//// ye bhi "Distribution pattern" jaisa hi haiii
+    // bss isme ek aisa haii ki hme lexicographically largest chahiye ...so, largest freq wale ki jagah largest character wala pehle daalenge
+    // as, hme har baar largest char chahiye, so max heap for getting largest char
+    // and hme har char ki freq bhi chahiye, so unordered_map to store freq
+    // algo -
+    // take the largest available char
+    // use it as much as allowed
+    // if it still remains, insert next smaller char to break the streak
+    // continue again
+
 class Solution {
     public String repeatLimitedString(String s, int repeatLimit) {
 
@@ -32,7 +42,7 @@ class Solution {
             // Use up to repeatLimit
             int use = Math.min(count1, repeatLimit);
 
-            // append character 'use' times
+            // append character use times
             for (int i = 0; i < use; i++) {
                 sb.append((char)(ch1 + 'a'));
             }
