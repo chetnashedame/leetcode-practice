5c9a77fd938c6fc990b28b499a6cd62fa5de6b9f
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-27 17:12:56 +0530

    Time: 6 ms (96.25%) | Memory: 47.7 MB (73.75%) - LeetSync

---
+++ 2817-minimum-cost-to-make-all-characters-equal/minimum-cost-to-make-all-characters-equal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long minimumCost(String s) {
+        long cost=0;
+        int n=s.length();
+
+        for(int i=1; i<n; i++){
+
+            if (s.charAt(i) != s.charAt(i-1)){
+                cost+=Math.min(i, n-i);
+            }
+        }
+        return cost;
+    }
+}
