055ff40f08d83bb4291d99299e1bbec729eb15b1
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-27 11:51:42 +0530

    Time: 104 ms (12.21%) | Memory: 53 MB (88.55%) - LeetSync

---
+++ 991-array-of-doubled-pairs/array-of-doubled-pairs.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canReorderDoubled(int[] arr) {
+
+        // Step 1: frequency map
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: sort by absolute value
+        Integer[] nums = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(nums, (a, b) -> Integer.compare(Math.abs(a), Math.abs(b)));
+
+        // Step 3: greedy pairing
+        for (int x : nums) {
+
+            if (map.get(x) == 0) continue;
+
+            int doubleVal = 2 * x;
+
+            if (map.getOrDefault(doubleVal, 0) <= 0) {
+                return false;
+            }
+
+            // use both(if pair exists then remove both)
+            map.put(x, map.get(x) - 1);
+            map.put(doubleVal, map.get(doubleVal) - 1);
+        }
+
+        return true;
+    }
+}
