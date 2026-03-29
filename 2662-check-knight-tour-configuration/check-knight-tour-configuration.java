a91142b4cbbbf4ea3534a3fc19fd5e089c5c70bf
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-03-29 17:40:26 +0530

    Time: 2 ms (6.53%) | Memory: 46.5 MB (22.06%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkValidGrid(int[][] grid) {
+        int n=grid.length;
+
+        int [][] pos= new int[n*n][2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pos[grid[i][j]] = new int[]{i, j};
+            }
+        }
+
+        if (pos[0][0] != 0 || pos[0][1] != 0) {
+            return false;
+        }
+
+        for (int i = 1; i < n * n; i++) {
+            int r1 = pos[i - 1][0];
+            int c1 = pos[i - 1][1];
+            int r2 = pos[i][0];
+            int c2 = pos[i][1];
+
+            int dr = Math.abs(r1 - r2);
+            int dc = Math.abs(c1 - c2);
+
+            if (!((dr == 2 && dc == 1) || (dr == 1 && dc == 2))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
