14ab473056a25f58f78af779555d7509c233f11a
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-04-18 12:11:08 +0530

    Time: 2 ms (16.06%) | Memory: 41.8 MB (93.82%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,15 @@
+/// i=10  can be writtn as 1 + 9, 2+8, 3+7 i.e  j to i-j where j is 1 to 9
+class Solution {
+    public int integerBreak(int n) {
+        int [] dp= new int[n+1];
+
+        dp[1]=1;                  
+
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<i; j++){
+                dp[i]= Math.max(dp[i], Math.max(j * (i-j), j * dp[i-j])); 
+            }
+        }
+        return dp[n];
+    }
+}
