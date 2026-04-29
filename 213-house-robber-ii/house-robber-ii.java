822a8e9f1282d281059628547d0e4751a76cbf77
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-04-29 20:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (22.87%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,31 +1,35 @@
-///// recursion + memoization (top down approach) same as house 1 just need to add 2 cases and two dp states
+///// bottoms up approch +memoization
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
         if (n == 1) return nums[0];
 
-        // case 1: exclude last
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-        int case1 = solve(nums, 0, n - 2, dp1);
+        // Case 1- include first, exclude last (0 → n-2)
+        int[] t = new int[n + 1];
+        t[0] = 0;
 
-        // case 2: exclude first
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2, -1);
-        int case2 = solve(nums, 1, n - 1, dp2);
+        for (int i = 1; i <= n - 1; i++) {
+            t[i] = Math.max(
+                t[i - 1],
+                nums[i - 1] + (i - 2 >= 0 ? t[i - 2] : 0)
+            );
+        }
+        int result1 = t[n - 1];
 
-        return Math.max(case1, case2);
-    }
+        // Case 2- exclude first, include last (1 → n-1)
+        t = new int[n + 1]; // reinitialize (Java doesn't have clear like vector)
+        t[0] = 0;
+        t[1] = 0;
 
-    private int solve(int[] nums, int i, int end, int[] dp) {
-        if (i > end) return 0;
+        for (int i = 2; i <= n; i++) {
+            t[i] = Math.max(
+                t[i - 1],
+                nums[i - 1] + (i - 2 >= 0 ? t[i - 2] : 0)  ///to avoid out of bound situation
+            );
+        }
+        int result2 = t[n];
 
-        if (dp[i] != -1) return dp[i];
-
-        int steal = nums[i] + solve(nums, i + 2, end, dp);
-        int skip = solve(nums, i + 1, end, dp);
-
-        return dp[i] = Math.max(steal, skip);
+        return Math.max(result1, result2);
     }
 }
