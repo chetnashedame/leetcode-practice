49ff578c72cac63d9ec2e2b88a22e58c1ede9b9d
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-04-29 17:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.53%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,24 +1,19 @@
-////// recursion+memoization approach (Top-down)
+/////// bottom up approch
 class Solution {
-    int dp[];
     public int rob(int[] nums) {
-        int n= nums.length;
-        dp=new int[n];
+    int n=nums.length;
 
-        Arrays.fill(dp, -1);
-        return solve(nums, 0);
-    }
-    
-    private int solve(int []nums, int i){
-        if (i>=nums.length) return 0;
+    int[]dp=new int[n+1];
 
-        if(dp[i]!=-1){
-            return dp[i];
-        }
+    dp[0]=0;
+    dp[1]=nums[0];
 
-        int steal=nums[i]+solve(nums, i+2);
-        int skip =solve(nums, i+1);
+    for(int i=2; i<=n; i++){
+        int steal= nums[i-1]+ dp[i-2];
+        int skip= dp[i-1];
 
-        return dp[i]=Math.max(steal, skip);
+        dp[i]=Math.max(steal, skip);
+    } 
+    return dp[n];
     }
 }
