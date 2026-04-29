086d2073d7dcb4a08d0783b839cbe80b7f213e7c
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-04-29 19:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (60.47%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,31 @@
+///// recursion + memoization (top down approach) same as house 1 just need to add 2 cases and two dp states
 class Solution {
     public int rob(int[] nums) {
-       int n= nums.length;
+        int n = nums.length;
 
-       if(n==1) return nums[0];
+        if (n == 1) return nums[0];
 
-       return Math.max(skipFirstOrLast(nums, 0, n-2), skipFirstOrLast(nums, 1, n-1));
+        // case 1: exclude last
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1, -1);
+        int case1 = solve(nums, 0, n - 2, dp1);
+
+        // case 2: exclude first
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2, -1);
+        int case2 = solve(nums, 1, n - 1, dp2);
+
+        return Math.max(case1, case2);
     }
 
-    private int skipFirstOrLast(int [] nums, int first, int last){
-        int prev2=0;
-        int prev1=0;
+    private int solve(int[] nums, int i, int end, int[] dp) {
+        if (i > end) return 0;
 
-        for(int i=first; i<=last; i++){
-            int curr= Math.max(prev1, nums[i]+prev2);  
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        if (dp[i] != -1) return dp[i];
+
+        int steal = nums[i] + solve(nums, i + 2, end, dp);
+        int skip = solve(nums, i + 1, end, dp);
+
+        return dp[i] = Math.max(steal, skip);
     }
 }
