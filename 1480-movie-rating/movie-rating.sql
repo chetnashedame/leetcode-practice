a5b6137ec11fc5bb672694ac19ede85860994c76
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-07-11 00:12:02 +0530

    Time: 1400 ms (45.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+(select u.name as results 
+from Users u
+join MovieRating m on u.user_id=m.user_id
+group by u.user_id 
+order by count(m.movie_id) desc, u.name asc
+limit 1)
+
+ union all 
+
+(select mo.title as results
+from Movies mo
+join MovieRating mr on mo.movie_id=mr.movie_id
+where mr.created_at between '2020-02-01' and '2020-02-29'
+group by mo.movie_id
+order by avg(mr.rating) desc, mo.title asc
+limit 1
+)
+;
