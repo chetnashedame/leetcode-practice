19e38a131b682dc94995f60605dddff06c8d9047
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-08-01 17:44:59 +0530

    Time: 177 ms (24.60%) | Memory: 96.3 MB (54.96%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
-        HashMap<String, Integer>map= new HashMap<>();
+        Set<String> set= new HashSet<>();  // set will remove duplicates
 
         for(int i=0; i+k <=s.length(); i++){ ///we don't want codes to be out og s.length()
-            String binaryCodes= s.substring(i, i+k);
-            map.put(binaryCodes, map.getOrDefault(binaryCodes, 0)+1);
+           set.add(s.substring(i, i+k));
         }
-        return map.size()==(1<<k); //1<<k will compute total no. of binarycodes of length k. << shifts left by 1 all the remaining values will be 0 
+        return set.size()==(1<<k); //1<<k will compute total no. of binarycodes of length k. << shifts left by 1 all the remaining values will be 0 
         //for e.g. 0001 -> 0010 -> 0100 -> 1000 thus, all the binary codes will be generated.
     }
 }
