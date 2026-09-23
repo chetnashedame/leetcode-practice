f9c53e6a49c13d7eec0f911e662d7a210c264182
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2026-09-23 15:59:57 +0530

    Time: 11 ms (96.09%) | Memory: 47.9 MB (13.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freqMap= new HashMap<>();
+        HashMap<Integer, Integer> freqMap= new HashMap<>(); //create a fre hashmap to store the freq of every num
 
         for(int num: nums){
             freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
         }
 
-        List<Integer>[] buckets= new List[nums.length+1];
+        List<Integer>[] buckets= new List[nums.length+1];  //create array of list to store nums having same freq 
 
         for(Map.Entry<Integer, Integer> entry: freqMap.entrySet()){
             int num= entry.getKey();
@@ -17,8 +17,8 @@ class Solution {
             buckets[freq].add(num);
         }
 
-        int []result= new int[k];
-        int index=0;
+        int []result= new int[k];  //take array to store res
+        int index=0;   //index = freq so we get at index 1 we have freq1 means nums that are 1  so if we loop backwords we get highest k freq 
         for(int freq= buckets.length-1; freq>=0 && index<k; freq--){
             if(buckets[freq]!=null){
                 for(int num:buckets[freq]){
